e4b3bfffaea5bf013a0ffd1eedfb97aba6251322
Rainer <rtmayagma@up.edu.ph>
2024-11-09 15:11:05 +0800

    lira update

--- LIRA Chatbot/actions/actions.py
+++ LIRA Chatbot/actions/actions.py
@@ -1,27 +1,26 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionSayShirtSize(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_confirm_details"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        first_name = tracker.get_slot("first_name")
+        last_name = tracker.get_slot("last_name")
+        email = tracker.get_slot("email")
+        service = tracker.get_slot("service")
+
+        if not (first_name or last_name or email or service):
+            dispatcher.utter_message(text="I don't know your shirt size.")
+        else:
+            dispatcher.utter_message(text=f"Name: {first_name} {last_name}, Email: {email}, Service: {service}. Is this correct?")
+        return []
