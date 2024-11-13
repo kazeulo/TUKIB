1dead30edfbf0c5cc283ce90745d3b5b7713159a
Kzlyr Shaira Manejo <kmanejo@up.edu.ph>
2024-11-14 02:01:08 +0800

    update home

--- src/components/Home.js
+++ src/components/Home.js
@@ -44,56 +44,84 @@ const Home = () => {
 					</div>
 				</div>{' '}
 				{/* end of banner */}
+
 				{/* our services section */}
-				<section
-					ref={servicesRef}
-					className='services'>
-					<h2>Our Services</h2>
-					<div className='services-container'>
-						<div className='service-card'>
+				<section ref={servicesRef} className="services">
+					<div className="sectionTitle text-center">
+						<h6>What we do</h6>
+						<h2>Our Services</h2>
+					</div>
+
+					<div className="container">
+						<div className="row">
+
+						{/* Service Card 1 */}
+						<div className="col-12 col-sm-6 col-md-3 mb-4">
+							<div className="service-card d-flex flex-column h-100 text-center">
 							<h3>Sample Processing</h3>
+							<div className="icon-container mb-3">
+								<i className="services-icon fas fa-flask"></i>
+							</div>
 							<p>
-								Clients can send their samples for sample processing. The
-								service will give you the Raw Data/Results generated from the
-								instrument and does not do data processing and interpretation
-								(No Certificate of Analysis). Samples should be ready for
-								processing and protocols should be provided upon submission of
-								sample (if applicable.)
+								Clients can send their samples for sample processing. The service will give you the Raw Data/Results generated from the instrument and does not do data processing and interpretation (No Certificate of Analysis). Samples should be ready for processing and protocols should be provided upon submission of sample (if applicable.)
 							</p>
+							</div>
 						</div>
-						<div className='service-card'>
+
+						{/* Service Card 2 */}
+						<div className="col-12 col-sm-6 col-md-3 mb-4">
+							<div className="service-card d-flex flex-column h-100 text-center">
 							<h3>Use of Equipment</h3>
+							<div className="icon-container mb-3">
+								<i className="services-icon fas fa-laptop"></i> {/* Use of Equipment Icon */}
+							</div>
 							<p>
-								Clients will be permitted to use the equipment of the laboratory
-								with the guidance of the laboratory personnel. Training is
-								required to use the equipment. Email or coordinate with RRC
-								personnel for more details.
+								Clients will be permitted to use the equipment of the laboratory with the guidance of the laboratory personnel. Training is required to use the equipment. Email or coordinate with RRC personnel for more details.
 							</p>
+							</div>
 						</div>
-						<div className='service-card'>
+
+						{/* Service Card 3 */}
+						<div className="col-12 col-sm-6 col-md-3 mb-4">
+							<div className="service-card d-flex flex-column h-100 text-center">
 							<h3>Training Service</h3>
+							<div className="icon-container mb-3">
+								<i className="services-icon fas fa-chalkboard-teacher"></i> {/* Training Service Icon */}
+							</div>
 							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-								Vestibulum ac mauris semper ex maximus luctus et in tellus. Sed
-								lacinia venenatis luctus. Nulla id arcu ipsum. Nulla vestibulum
-								tincidunt urna sit amet lacinia. Sed accumsan ullamcorper
-								pulvinar. Suspendisse accumsan ex eu elit luctus aliquam.
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac mauris semper ex maximus luctus et in tellus. Sed lacinia venenatis luctus. Nulla id arcu ipsum. Nulla vestibulum tincidunt urna sit amet lacinia. Sed accumsan ullamcorper pulvinar. Suspendisse accumsan ex eu elit luctus aliquam.
 							</p>
+							</div>
 						</div>
-						<div className='service-card'>
+
+						{/* Service Card 4 */}
+						<div className="col-12 col-sm-6 col-md-3 mb-4">
+							<div className="service-card d-flex flex-column h-100 text-center">
 							<h3>Use of Facilities</h3>
+							<div className="icon-container mb-3">
+								<i className="services-icon fas fa-building"></i> {/* Use of Facilities Icon */}
+							</div>
 							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-								Vestibulum ac mauris semper ex maximus luctus et in tellus. Sed
-								lacinia venenatis luctus. Nulla id arcu ipsum. Nulla vestibulum
-								tincidunt urna sit amet lacinia. Sed accumsan ullamcorper
-								pulvinar. Suspendisse accumsan ex eu elit luctus aliquam.
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac mauris semper ex maximus luctus et in tellus. Sed lacinia venenatis luctus. Nulla id arcu ipsum. Nulla vestibulum tincidunt urna sit amet lacinia. Sed accumsan ullamcorper pulvinar. Suspendisse accumsan ex eu elit luctus aliquam.
 							</p>
+							</div>
 						</div>
 					</div>
-				</section>{' '}
-				{/* end of our services section */}
+					</div>
+				</section> {/* end of our services section */}
+
+				{/* statistic counter us section */}
+
+				<section className='statisticCounter'>
+			
+				</section>
+				{/* end of statistic counter section */}
+
 				{/* contact us section */}
+				<div className='sectionTitle'>
+					<h6>Need Help?</h6>
+					<h2>Contact Us</h2>
+				</div>
 				<ContactForm />
 				{/* end of contact us section */}
 				<EventCalendar />
